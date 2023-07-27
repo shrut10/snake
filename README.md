027a008e76ed21ee2fd3744f74993308c359b84a
Jayashruthi Rajesh Babu <rjayashruthi@yahoo.com>
2023-07-28 00:18:46 +0100

    Add new features to the Snake Game

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# snake
-# snake
+<h1 align="center">🐍 Snake Game 🎮</h1>
+
+<p align="center">
+  Welcome to the Snake Game! This is a simple and fun implementation of the classic Snake game using Python and Pygame.
+</p>
+
+<h2>Features 🚀</h2>
+
+1. **Snake Movement**: Control the snake using the arrow keys to eat the food and grow in size.
+
+2. **Food**: 🍉 Neon pink food pieces appear on the screen for the snake to eat. Different types of food have varying point values.
+
+3. **Score Display**: 📊 The game screen shows the player's current score, indicating the number of food pieces eaten.
+
+4. **High Score**: 🏆 The game keeps track of the player's best score across multiple sessions.
+
+5. **Level System**: 🎮 As the player's score increases, the game speed and difficulty level increase, offering a sense of progression.
+
+6. **Countdown Timer**: ⏳ Each game round lasts for 30 seconds, adding a time-based challenge to the gameplay.
+
+7. **Game Over Popup**: 💥 When the snake collides with itself, a popup window appears displaying "Game Over" along with buttons to "Try Again" or "Exit Game." Players can continue playing or exit the game as they like.
+
+8. **Power-Ups (Optional)**: ⚡ You can add power-ups to the game, such as temporary speed boost or invincibility, to enhance the gaming experience further.
+
+<h2>How to Play 🎮</h2>
+
+1. Clone the repository or download the source code.
+
+2. Ensure you have Python and Pygame installed on your system.
+
+3. Run the `snake_game.py` script using Python:
+
+python snake_game.py
+
+
+4. Use the arrow keys to control the snake's direction (Up, Down, Left, Right).
+
+5. Eat the neon pink food to increase your score and grow the snake.
+
+6. Avoid colliding with the walls or the snake's body.
+
+7. Try to achieve the highest score within the 30-second time limit.
+
+8. When the game ends, a popup will display showing your score, high score, and options to try again or exit the game.
+
+<h2>Customize the Game 🎨</h2>
+
+You can further customize the game by tweaking the game constants such as window size, cell size, colors, countdown duration, and more in the `snake_game.py` file. Feel free to experiment and make the game uniquely yours!
+
+<h2>Acknowledgments 🙏</h2>
+
+The Snake Game was created based on Python and Pygame tutorials and is a fun project to learn and practice game development concepts.
+
+<h2>License 📜</h2>
+
+This project is licensed under the [MIT License](LICENSE).
--- snake_game.py
+++ snake_game.py
@@ -13,7 +13,6 @@ CELL_SIZE = 20
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 NEON_PINK = (255, 0, 255)
-RED = (255, 0, 0)
 
 # Create the game window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -54,6 +53,7 @@ class Snake:
 class Food:
     def __init__(self):
         self.position = self.random_position()
+        self.points = 1
 
     def random_position(self):
         x = random.randrange(0, WIDTH, CELL_SIZE)
@@ -62,9 +62,46 @@ class Food:
 
     def respawn(self):
         self.position = self.random_position()
+        self.points = random.choice([1, 2, 5])  # Varying point values for different food types
 
     def draw(self, surface):
         pygame.draw.rect(surface, NEON_PINK, (self.position[0], self.position[1], CELL_SIZE, CELL_SIZE))
+
+def show_game_over_screen(score, high_score):
+    font = pygame.font.SysFont(None, 50)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:  # 'Enter' key to try again
+                    return True
+                elif event.key == pygame.K_ESCAPE:  # 'Escape' key to exit
+                    return False
+
+        # Clear the screen with a black background
+        screen.fill(BLACK)
+
+        # Render the "Game Over" text
+        game_over_text = font.render("Game Over", True, GREEN)
+        screen.blit(game_over_text, (WIDTH // 2 - game_over_text.get_width() // 2, HEIGHT // 2 - game_over_text.get_height() // 2))
+
+        # Render the score and high score text
+        score_text = font.render(f"Score: {score}", True, NEON_PINK)
+        screen.blit(score_text, (WIDTH // 2 - score_text.get_width() // 2, HEIGHT // 2 + 20))
+        
+        high_score_text = font.render(f"High Score: {high_score}", True, NEON_PINK)
+        screen.blit(high_score_text, (WIDTH // 2 - high_score_text.get_width() // 2, HEIGHT // 2 + 70))
+
+        # Draw the "Try Again" and "Exit Game" instructions
+        instructions_text = font.render("Press Enter to Try Again or Esc to Exit", True, NEON_PINK)
+        screen.blit(instructions_text, (WIDTH // 2 - instructions_text.get_width() // 2, HEIGHT // 2 + 120))
+
+        # Update the display
+        pygame.display.flip()
 
 def main():
     # Create the snake and set the initial direction
@@ -73,6 +110,19 @@ def main():
 
     # Create the food
     food = Food()
+
+    # Game variables
+    score = 0
+    high_score = 0
+    level = 1
+    speed = 10
+    countdown_duration = 30  # 30 seconds countdown timer
+
+    # Countdown timer font
+    timer_font = pygame.font.SysFont(None, 50)
+
+    # Countdown timer variables
+    start_time = pygame.time.get_ticks()
 
     # Game loop
     while True:
@@ -98,14 +148,36 @@ def main():
         # Move the snake and check for collisions
         snake.move()
         if snake.check_collision():
-            print("Game Over!")
-            pygame.quit()
-            sys.exit()
+            if score > high_score:
+                high_score = score
+            if not show_game_over_screen(score, high_score):
+                pygame.quit()
+                sys.exit()
+            # Reset the game after the 'Game Over' pop-up
+            snake.body = [(WIDTH // 2, HEIGHT // 2)]
+            dx, dy = 1, 0
+            food.respawn()
+            score = 0
+            level = 1
+            speed = 10
+            start_time = pygame.time.get_ticks()  # Reset the countdown timer
 
         # Check if the snake eats the food
         if snake.body[0] == food.position:
             snake.grow()
+            score += food.points
+            if score > high_score:
+                high_score = score
             food.respawn()
+
+            # Increase speed and level based on the player's score
+            if score >= 10:
+                speed = 15
+                level = 2
+            if score >= 20:
+                speed = 20
+                level = 3
+            # Add more levels as needed
 
         # Clear the screen with a black background
         screen.fill(BLACK)
@@ -114,11 +186,40 @@ def main():
         snake.draw(screen)
         food.draw(screen)
 
+        # Render the score, level, and countdown timer on the game screen
+        font = pygame.font.SysFont(None, 30)
+        score_text = font.render(f"Score: {score} | Level: {level}", True, NEON_PINK)
+        screen.blit(score_text, (10, 10))
+
+        # Calculate the remaining time in seconds
+        current_time = pygame.time.get_ticks()
+        elapsed_seconds = (current_time - start_time) // 1000
+        time_remaining = max(0, countdown_duration - elapsed_seconds)
+
+        timer_text = timer_font.render(f"Time Remaining: {time_remaining} s", True, NEON_PINK)
+        screen.blit(timer_text, (WIDTH - timer_text.get_width() - 10, 10))
+
+        # Check if time is up
+        if time_remaining <= 0:
+            if score > high_score:
+                high_score = score
+            if not show_game_over_screen(score, high_score):
+                pygame.quit()
+                sys.exit()
+            # Reset the game after the 'Game Over' pop-up
+            snake.body = [(WIDTH // 2, HEIGHT // 2)]
+            dx, dy = 1, 0
+            food.respawn()
+            score = 0
+            level = 1
+            speed = 10
+            start_time = pygame.time.get_ticks()  # Reset the countdown timer
+
         # Update the display
         pygame.display.flip()
 
         # Set the frame rate
-        clock.tick(10)  # Adjust this value to change the snake's speed
+        clock.tick(speed)  # Adjust this value to change the snake's speed
 
 if __name__ == "__main__":
     main()
